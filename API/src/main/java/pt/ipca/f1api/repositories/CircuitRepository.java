af42e85598e826c659b17cbb7bfa731a997e5728
Tiago Silva <tiago.silva@impactzero.pt>
2018-11-23 20:11:04 +0000

    update

--- API/src/main/java/pt/ipca/f1api/repositories/CircuitRepository.java
+++
@@ -1,8 +0,0 @@
-package pt.ipca.f1api.repositories;
-
-import pt.ipca.f1api.models.Circuit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CircuitRepository extends JpaRepository<Circuit, Long> {
-
-}

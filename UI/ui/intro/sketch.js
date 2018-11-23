a21e352a650e431674c21ab8aba782e1b0f34ac0
Tiago Silva <tiago.silva@impactzero.pt>
2018-11-23 21:58:59 +0000

    p5js

---
+++ UI/ui/intro/sketch.js
@@ -0,0 +1,28 @@
+var canvas;
+var ipcaLogo;
+var f1Logo;
+var circuits;
+
+function setup() {
+   canvas = createCanvas(window.innerWidth, window.innerHeight);
+
+   // Load images to page
+   //ipcaLogo = loadImage("../../img/ipca-logo.png");
+   circuits = loadJSON("http://localhost:3000/api/circuits");
+   console.log(circuits);
+}
+
+function draw() {
+  background(0);
+  stroke(255);
+  triangle(0, 0, 0, height, width/2, 0);
+}
+
+
+window.onresize = function() {
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  canvas.size(w,h);
+  width = w;
+  height = h;
+};
